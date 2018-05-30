6760753ea34392f3311a0e02a38641d362f3546d
SandeepThakare <sandythakare9@gmail.com>
2018-05-30 14:28:42 +0530

    express add

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-// Source: howtonode.org/hello-node
+import express from 'express';
 
-// Load the http module to create an http server.
-var http = require('http');
+let app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+app.get('/', (req, res) => {
+	res.send('Hello there !!!');
 });
 
+app.use((req, res) => {
+	res.sendStatus(400);
+});
+
+// Configure our HTTP server to respond with Hello World to all requests.
+// var server = createServer((request, response) => {
+// 	response.writeHead(200, { 'Content-Type': 'text/plain' });
+// 	response.end('Hello World\n');
+// });
+
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+app.listen(8000);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+console.log('Server running at http://127.0.0.1:8000/');
