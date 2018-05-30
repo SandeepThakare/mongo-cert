bb5a82e74dbd6399a895b7ded155fc967d42debc
SandeepThakare <sandythakare9@gmail.com>
2018-05-30 15:27:41 +0530

    done with mongo connect

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { MongoClient } from 'mongodb';
 let app = express();
 
 import engines from 'consolidate';
@@ -8,16 +8,32 @@ app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
-	res.render('hello', {'name': 'Sandeep'});
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+
+	if (err) {
+		console.log('Unable to conect with db. Error JSON: ', JSON.stringify(err, undefined, 2));
+	}
+
+	let db = client.db('video');
+
+	app.get('/', (req, res) => {
+		db.collection('movies').find({}).toArray()
+			.then((result) => {
+				res.render('movies', {'movies': result});
+			})
+			.catch((err) => {
+				console.log('Unable to conect with db. Error JSON: ', JSON.stringify(err, undefined, 2));
+			});
+	}); 
+
+	app.use((req, res) => {
+		res.sendStatus(400);
+	});
+
+	// Listen on port 8000, IP defaults to 127.0.0.1
+	app.listen(8000);
+
+	// Put a friendly message on the terminal
+	console.log('Server running at http://127.0.0.1:8000/');
+
 });
-
-app.use((req, res) => {
-	res.sendStatus(400);
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-app.listen(8000);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:8000/');
