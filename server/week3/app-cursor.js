4818fa6e352ccb01133085b0bed00ffc92ad10b1
SandeepThakare <sandythakare9@gmail.com>
2018-06-13 18:15:27 +0530

    as 4

--- server/week3/app-cursor.js
+++ server/week3/app-cursor.js
@@ -10,15 +10,16 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
 
 	assert.equal(err, null);
 	console.log('Successfully connected to MongoDB.');
-
+	
 	let db = client.db('crunchbase');
-    
-	var query = queryDocument(options);
-	var projection = projectionDocument(options);
 
-	var cursor = db.collection('companies').find(query);
-	cursor.project(projection);
-    
+	var query = queryDocument(options);
+	var projection = {'_id': 0,
+		'name': 1,
+		'offices.country_code': 1,
+		'ipo.valuation_amount': 1};
+
+	var cursor = db.collection('companies').find(query, projection);
 	var numMatches = 0;
 
 	cursor.forEach(
@@ -41,10 +42,27 @@ function queryDocument(options) {
 
 	console.log(options);
     
-	var query = {};
+	var query = {
+		'founded_year': {
+			'$gte': options.firstYear,
+			'$lte': options.lastYear
+		}
+	};
 
-	if ('overview' in options) {
-		query.overview = {'$regex': options.overview, '$options': 'i'};
+	if ('employees' in options) {
+		query.number_of_employees = { '$gte': options.employees };
+	}
+    
+	if ('ipo' in options) {
+		if (options.ipo == 'yes') {
+			query['ipo.valuation_amount'] = {'$exists': true, '$ne': null};
+		} else if (options.ipo == 'no') {
+			query['ipo.valuation_amount'] = null;
+		}               
+	}
+
+	if ('country' in options) {
+		query['offices.country_code'] = options.country;
 	}
     
 	return query;
@@ -52,30 +70,21 @@ function queryDocument(options) {
 }
 
 
-function projectionDocument(options) {
-
-	var projection = {
-		'_id': 0,
-		'name': 1,
-		'founded_year': 1,
-		'overview': 1
-	};
-
-	return projection;
-}
-
-
 function commandLineOptions() {
 
 	var cli = commandLineArgs([
-		{ name: 'overview', alias: 'o', type: String }
+		{ name: 'firstYear', alias: 'f', type: Number },
+		{ name: 'lastYear', alias: 'l', type: Number },
+		{ name: 'employees', alias: 'e', type: Number },
+		{ name: 'ipo', alias: 'i', type: String },
+		{ name: 'country', alias: 'c', type: String }
 	]);
     
 	var options = cli.parse();
-	if (Object.keys(options).length < 1) {
+	if ( !(('firstYear' in options) && ('lastYear' in options))) {
 		console.log(cli.getUsage({
 			title: 'Usage',
-			description: 'You must supply at least one option. See below.'
+			description: 'The first two options below are required. The rest are optional.'
 		}));
 		process.exit();
 	}
