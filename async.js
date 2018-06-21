f3899cc1840bda39eb419d8df0e3beb4478f0540
SandeepThakare <sandythakare9@gmail.com>
2018-06-21 15:51:40 +0530

    async.js

---
+++ async.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+const start = Date.now();
+console.log(Date.now());
+
+function doRequest() {
+	https.request('https://www.google.com', res => {
+
+		res.on('data', () => { });
+		res.on('end', () => {
+			console.log(Date.now() - start);
+		});
+	}).end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
